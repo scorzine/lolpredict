ecb0d2170fee3a995245e01d956ac60782717535
Katie <33009876+krosengrant@users.noreply.github.com>
2018-05-10 14:06:56 -0700

    Dictionary for classes and subclasses
    7 Classes + 11 Subclasses = 18 total

---
+++ class_sublcass_dict.py
@@ -0,0 +1,20 @@
+classes_and_subclasses = {
+        "Controller"  :  1,           # CLASSES (values 1-7)
+        "Fighter"     :  2, 
+        "Mage"        :  3,
+        "Marksman"    :  4, 
+        "Slayer"      :  5, 
+        "Specialist"  :  6,           # Note: "Specialist" is also a subclass
+        "Tank"        :  7,
+        "Artillery"   :  8,           # SUBCLASSES (values 8-18)
+        "Assassin"    :  9,
+        "Battlemage"  : 10,
+        "Burst"       : 11,
+        "Catcher"     : 12,
+        "Diver"       : 13,
+        "Enchanter"   : 14,
+        "Juggernaut"  : 15,
+        "Skirmisher"  : 16,
+        "Vanguard"    : 17,
+        "Warden"      : 18
+    }
